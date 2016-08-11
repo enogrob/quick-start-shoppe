2e2afc1ae1d93dfee0e315bcc61f7cdf70fadaa4
Roberto Nogueira <enogrob@gmail.com>
2016-08-11 17:38:03 -0300

    performed Setting up Shoppe

---
+++ README.md
@@ -0,0 +1,19 @@
+```
+Roberto Nogueira  BSd EE, MSd CE
+Solution Integrator Experienced - Certified by Ericsson
+```
+
+# Quick Start Shoppe
+
+![ebook_cover](images/shoppe.png)
+
+## Contents
+
+```
+Contents
+
+[x] Setting up Shoppe
+[ ] Displaying your product catalog
+[ ] Customer baskets
+[ ] Checking out
+```
---
+++ widget_shop/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WidgetShop::Application.config.secret_key_base = '55412a37e5ef49109586ce7ede1d80f470dd00687b16ab45cbf4c1c27fab5c87445b9f8b8ef798c8d8a7ee691b27b0f23280f71d8fbab2b64a025649a1202307'
